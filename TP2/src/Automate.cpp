0a941d2a6f777fc3a867914e8db11cca33b094c1
Alex <alex.hua@polymtl.ca>
2020-12-03 02:09:00 -0500

    lots of work indeed

--- TP2/src/Automate.cpp
+++ TP2/src/Automate.cpp
@@ -27,24 +27,49 @@ void Automate::creerLexique(const std::string fichierText){
     
 }
 
-void Automate::creerVerif(std::vector<std::string> mot){
+void Automate::creerVerif(){
     etat_ = 0;
     int compteErreur = 0;
+    std::vector<std::string> mot;
 
     switch(etat_){
         case 0:
-            if (mot[0] == dictionnaire_[0]){
-                etat_ = 1;
+            cout << "Entrer votre reponse:"
+            cin >> mot;
+
+            if(mot == nullptr){
+                cout << "ce mot ne peut pas etre utilise"
+                etat_ = 0;
             }
             else{
-                compteErreur++;
-                for(int j = 0; j < dictionnaire_.size(); j++){
-                    if(mot[0] == dictionnaire_[j]){
-                        cout << "Lettre 1 est au mauvais endroit"
-                    }
+                etat_ = 1;
+            }
+        case 1:
+            for(int i = 0; i < dictionnaire_.size(); i++){}
+                if (mot[i] == dictionnaire_[i]){
+                    compteErreur++
                 }
             }
-        case 1:        
+            cout << "il y a " << compteErreur << "erreur";
+            if(compteErreur == 0){
+                etat_ = 2;
+            }
+            else{
+                for(int i = 0; i < mot.size(); i++){
+                    for(int j = 0; j < dictionnaire_.size(); j++){
+                        if(i != j){
+                            if(mot[i] == dictionnaire_[j]){
+                            cout << "la lettre " << i - 1 << " est au mauvais endroit";        
+                            }
+                        }
+                    }
+                }
+                etat_ = 0;
+            }
+
+        case 2:
+            cout << "vous avez gagnez";
+
     }
     
 }
